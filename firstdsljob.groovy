8fb4ed20d3397fc7d63c86af3f5d2ddfa4efbf68
nazar2003 <32235606+nazar2003@users.noreply.github.com>
2017-10-22 20:17:50 +0530

    Update firstdsljob.groovy

--- firstdsljob.groovy
+++ firstdsljob.groovy
@@ -33,3 +33,16 @@ freeStyleJob('testing1 from git') {
           }
    
 }
+pipelineJob('my pipe line job') {
+definition {
+cpsScm {
+scm{
+git{
+remote{
+url(https://github.com/nazar2003/New_Repo.git)
+       }
+   }
+   }
+        }
+          }
+                                  }
